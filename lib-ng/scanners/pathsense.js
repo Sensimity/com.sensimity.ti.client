66f65b36209003e50e0f7fd7d7c2ccba763bef60
William Rijksen <williamrijksen@gmail.com>
2016-09-14 11:21:59 +0200

    make the usage of the pathsense module optional

--- lib-ng/scanners/pathsense.js
+++ lib-ng/scanners/pathsense.js
@@ -1,11 +1,11 @@
-import PSModule from 'com.sensimity.ti.pathsense';
 import beaconMapper from '../mapper/pathsense/beacon';
 
 class Pathsense {
   constructor(beaconHandler) {
+    this.PSModule = require('com.sensimity.ti.pathsense');
     this.beaconHandler = beaconHandler;
     this.enteredRegion = this.enteredRegion.bind(this);
-    PSModule.addEventListener('enteredRegion', this.enteredRegion);
+    this.PSModule.addEventListener('enteredRegion', this.enteredRegion);
   }
 
   enteredRegion(geofenceRegion) {
@@ -14,22 +14,22 @@ class Pathsense {
   }
 
   startMonitoring(region) {
-    PSModule.startMonitoringForRegion(region);
+    this.PSModule.startMonitoringForRegion(region);
   }
 
   /**
   * Sort geofence-regions by distance inside a defined radius from a predefined location.
   */
   sortRegionsByDistance(regions, location, defaultRadius = 5000) {
-    return PSModule.sortRegionsByDistance(regions, location, defaultRadius);
+    return this.PSModule.sortRegionsByDistance(regions, location, defaultRadius);
   }
 
   stop() {
-    PSModule.stopMonitoringAllRegions();
+    this.PSModule.stopMonitoringAllRegions();
   }
 
   destruct() {
-    PSModule.removeEventListener('enteredRegion', this.enteredRegion);
+    this.PSModule.removeEventListener('enteredRegion', this.enteredRegion);
   }
 }
 
