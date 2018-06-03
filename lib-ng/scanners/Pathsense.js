f3cc23f4da369006fce0840028fcda13c346296d
Jeroen van Dijk <jeroen@enrise.com>
2018-06-03 10:49:26 +0200

    Prevent crash on destroy and reload afterwards

--- lib-ng/scanners/Pathsense.js
+++ lib-ng/scanners/Pathsense.js
@@ -1,46 +1,39 @@
 import BaseScanner from './Base';
 import mapper from '../mapper/pathsense';
 
+const proxy = require('com.sensimity.ti.pathsense');
+
 export default class Pathsense extends BaseScanner {
   constructor(beaconHandler) {
     super(mapper, null, beaconHandler);
     try {
-      this.Pathsense = require('com.sensimity.ti.pathsense');
-      this.Pathsense.addEventListener('enteredRegion', this.enteredRegion);
-      this.Pathsense.addEventListener('exitedRegion', this.exitedRegion);
+      proxy.addEventListener('enteredRegion', this.enteredRegion);
+      proxy.addEventListener('exitedRegion', this.exitedRegion);
     } catch (e) {
       Ti.API.info('Could not start geofence-scan, please include the com.sensimity.ti.pathsense module');
     }
   }
 
   startMonitoring(region) {
-    if (this.Pathsense) {
-      this.Pathsense.startMonitoringForRegion(region);
-    }
+    proxy.startMonitoringForRegion(region);
   }
 
   stop() {
-    if (this.Pathsense) {
-      this.Pathsense.stopMonitoringAllRegions();
-    }
+    proxy.stopMonitoringAllRegions();
   }
 
   /**
   * Sort geofence-regions by distance inside a defined radius from a predefined location.
   */
   sortRegionsByDistance(regions, location, defaultRadius = 5000) {
-    if (this.Pathsense) {
-      return this.Pathsense.sortRegionsByDistance(regions, location, defaultRadius);
-    }
+    return proxy.sortRegionsByDistance(regions, location, defaultRadius);
   }
 
   destruct() {
-    if (this.Pathsense) {
-      if (Ti.Platform.osname === 'android') {
-        this.Pathsense.destroy();
-      }
-      this.Pathsense.removeEventListener('enteredRegion', this.enteredRegion);
-      this.Pathsense.removeEventListener('exitedRegion', this.exitedRegion);
+    if (Ti.Platform.osname === 'android') {
+      proxy.destroy();
     }
+    proxy.removeEventListener('enteredRegion', this.enteredRegion);
+    proxy.removeEventListener('exitedRegion', this.exitedRegion);
   }
 }
